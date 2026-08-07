fd50fc6afcd70ae5e56f221ccfd9dae8c7ac5b09
fon.suteara <fon.suteara@gmail.com>
2026-08-07 14:32:36 +0700

    Add more animal

---
+++ day-3-zoo.js
@@ -0,0 +1,20 @@
+//Add new animals to the zoo
+
+const url = 'https://api.api-ninjas.com/v1/animals?name=';
+const apiKey = 'UGFPhwZa6MZMzWwt5RHyBe75J27MCZ7wnHWrWuOe';
+const animalName = 'lion';
+async function getPost () {
+    try {
+        const response = await fetch(url+animalName, {
+            method: 'GET',
+            headers: {
+                'X-Api-Key': apiKey
+            }
+        });
+        const data = await response.json();
+        console.log(data[0])
+    } catch (error) {
+        console.error("Somthing went wrong!", error);
+    }
+}
+getPost();
